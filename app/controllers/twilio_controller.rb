1fd63a988efb8606d4461d6978d3fa3b087ad201
Sean Cosentino <sparkcollaboration@gmail.com>
2017-12-15 15:07:52 -0500

    demo line set up

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -5,9 +5,9 @@ require 'sanitize'
 class TwilioController < ApplicationController
 
 JORDAN = '+14156016003'
-JOSE = '+13053890082'
 JEFF = '+14152691903'
-ERICK = '+13054848553'
+SEAN = '+13057266671'
+
 
 
 
@@ -17,8 +17,8 @@ ERICK = '+13054848553'
 
 
     def ivr_welcome
-      welcome = 'Welcome to Fresh Import Group! Specialists in bringing the best produce from Central America to the United States.'
-      message = 'TO REACH A CUSTOMER SERVICE REP, PRESS 1. TO FOLLOW UP ON A CURRENT ORDER, PRESS 2. TO PLACE A NEW ORDER, PRESS 3. TO REACH JORDAN TOMBACK, PRESS 4. TO REACH ERICK RODAS, PRESS 5, TO REACH JOSE SARTI, PRESS 6'
+      welcome = 'Good Day, you have reached the offices of Feldman and Shapiro. '
+      message = 'we cannot get to the phone right now please push the Extension number you need or 9 for the operator. For Robert Feldmand press 1, For shapiro press 2, for accounting press 3, paralegal, 4, reception, 5.'
       response = Twilio::TwiML::VoiceResponse.new do |r|
         r.gather numDigits: '1', action: menu_path do |g|
           g.say welcome, voice: 'man', language: 'en-GB'
@@ -47,12 +47,12 @@ ERICK = '+13054848553'
 
     case user_selection
     when '1' then twiml_connect('One moment please.', JORDAN)
-    when '2' then twiml_connect('One moment please.', JOSE)
-    when '3' then twiml_connect('One moment please.', JEFF)
+    when '2' then twiml_connect('One moment please.', JEFF)
+    when '3' then twiml_connect('One moment please.', SEAN)
     when '4' then twiml_connect('One moment please.', JORDAN)
-    when '5' then twiml_connect('One moment please.', ERICK)
-    when '6' then twiml_connect('One moment please.', JOSE)
-    when '9' then twiml_connect('One moment while we connect you.', JORDAN)
+    when '5' then twiml_connect('One moment please.', JEFF)
+    when '6' then twiml_connect('One moment please.', SEAN)
+    when '9' then twiml_connect('One moment while we connect you.', SEAN)
       else else_statement
     end
   end
